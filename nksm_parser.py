c1960696723ed8e0ea7afca88f7385610426fb17
newnakashima <newnakashima2014@gmail.com>
2018-10-08 11:06:48 +0900

    if文をネストできる

--- nksm_parser.py
+++ nksm_parser.py
@@ -27,29 +27,66 @@ class Parser:
 
     def parse_if(self):
         lines = self.template.split("\n")
+        self.text = '\n'.join(self.__parse_if(lines))
+
+    def __parse_if(self, lines):
+        indent = ''
         ignore_flg = False
+        inside_if = False
+        raw_flg = False
         result_lines = []
+        nested_lines = []
+        nested_count = 0
+        ind_reg = re.compile('^(\s*)\S')
+        if_reg = re.compile('^\s*{{\s*(r)?if\s*(\w+)\s*}}$')
+        fi_reg = re.compile('^\s*{{\s*fi\s*}}$')
         for line in lines:
-            if_reg = re.compile('^{{\s*if\s*(.+)\s*}}$')
             if_matched = if_reg.match(line)
-            fi_reg = re.compile('^{{\s*fi\s*}}$')
             fi_matched = fi_reg.match(line)
             if if_matched != None:
+                ind_matched = ind_reg.match(line)
+                if (ind_matched != None and not inside_if):
+                    # トップレベルのif文行の場合
+                    indent = ind_matched.group(1)
+                    raw_flg = if_matched.group(1) != None
+                if inside_if:
+                    # ifブロック内にif文がある => ネスト
+                    nested_count += 1
+                    if not raw_flg:
+                        # ネスト内のインデントは最初のif文行にそろえる
+                        line = re.sub('^\s*', indent, line)
+                    nested_lines.append(line)
+                    continue
+                inside_if = True
                 # if文の行の場合は出力行に含めない
-                if not self.variables[if_matched.group(1)]:
+                if not self.variables[if_matched.group(2)]:
                     # ifの条件がFalseのときは無視フラグを立てる
                     ignore_flg = True
                 continue
             if fi_matched != None:
+                if nested_count > 0:
+                    nested_count -= 1
+                    if nested_count == 0:
+                        nested_lines.append(line)
+                        result_lines.extend(self.__parse_if(nested_lines))
+                    continue
+                inside_if = False
                 # if文から抜けるときは無視フラグ解除。出力行に含めない
                 ignore_flg = False
                 continue
             if ignore_flg:
                 # if文の中かつ条件に一致しない場合は出力しない
                 continue
+            if nested_count > 0:
+                # ネスト内の普通の行
+                nested_lines.append(line)
+                continue
+            if inside_if and not raw_flg and nested_count == 0:
+                # if文行のインデントを引き継ぐ
+                line = re.sub('^\s*', indent, line)
             # それ以外の場合は普通に出力
             result_lines.append(line)
-        self.text = '\n'.join(result_lines)
+        return result_lines
 
     def parse_variable(self):
         result_str = self.text
